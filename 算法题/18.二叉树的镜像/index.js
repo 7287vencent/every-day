6c515fcbbb2685de92e85ea13ef5309009d78cc3
7287vencent <1327920149@qq.com>
2020-02-28 16:51:46 +0800

    feat(算法题): 添加 18

---
+++ 算法题/18.二叉树的镜像/index.js
@@ -0,0 +1,16 @@
+/* function TreeNode(x) {
+    this.val = x;
+    this.left = null;
+    this.right = null;
+} */
+function Mirror (root) {
+  // write code here
+  if (root === null) {
+    return
+  }
+  let temp = root.left
+  root.left = root.right
+  root.right = temp
+  Mirror(root.left)
+  Mirror(root.right)
+}
