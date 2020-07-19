6806a3b6e770b8b07fff0ca505d65f0f79f493d7
7287vencent <1327920149@qq.com>
2020-07-19 20:25:16 +0800

    fix(其他): 修改 选择法排序

---
+++ 排序算法/选择法排序/index.js
@@ -0,0 +1,20 @@
+function selectionSort (arr) {
+  var len = arr.length;
+  var minIndex, temp;
+  console.time('选择排序耗时');
+  for (let i = 0; i < arr.length - 1; i++) {
+    minIndex = i
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j
+      }
+    }
+    temp = arr[minIndex]
+    arr[minIndex] = arr[i]
+    arr[i] = temp
+  }
+  console.timeEnd('选择排序耗时');
+  return arr;
+}
+var arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
+console.log(selectionSort(arr));//[2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
