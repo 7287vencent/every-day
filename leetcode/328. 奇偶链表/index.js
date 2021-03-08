e8561bc494a7e2dbeadd592cb91103820e005f71
7287vencent <1327920149@qq.com>
2021-03-08 20:39:53 +0800

    feat(leetcode): feat(leetcode): 328

---
+++ leetcode/328. 奇偶链表/index.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function (head) {
+  if (!head) {
+    return head;
+  }
+  let evenHead = head.next;
+  let odd = head,
+    even = evenHead;
+  while (even && even.next) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+  odd.next = evenHead;
+  return head;
+};
