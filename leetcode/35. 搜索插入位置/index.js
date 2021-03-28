a1cf3bc482bbadf3e9454f65706aaa8a1727117a
7287vencent <1327920149@qq.com>
2021-03-28 20:50:07 +0800

    feat(leetcode): 35

---
+++ leetcode/35. 搜索插入位置/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let ans = nums.length;
+  while (left <= right) {
+    let mid = (left + right) >> 1;
+    if (target <= nums[mid]) {
+      ans = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return ans;
+};
