8d75f98eafc8d1ce3b7ea443258e5dc9c1b0a5b1
7287vencent <1327920149@qq.com>
2021-04-16 21:56:17 +0800

    feat(leetcode): 680

---
+++ leetcode/680. 验证回文字符串 Ⅱ/index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function (s) {
+  const fn = (s, flag) => {
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right && s[left] === s[right]) {
+      left++;
+      right--;
+    }
+    if (right <= left) {
+      return true;
+    }
+    if (flag) {
+      return (
+        fn(s.slice(left, right), false) ||
+        fn(s.slice(left + 1, right + 1), false)
+      );
+    }
+    return false;
+  };
+  return fn(s, true);
+};
