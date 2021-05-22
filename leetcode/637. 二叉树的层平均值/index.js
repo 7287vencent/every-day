72ae1796e63f40fba8a17e66155d54cf5e5cac22
7287vencent <1327920149@qq.com>
2021-05-22 19:23:20 +0800

    feat(leetcode): 637

---
+++ leetcode/637. 二叉树的层平均值/index.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function (root) {
+  let average = [];
+  let queue = [root];
+  while (queue.length > 0) {
+    let sum = 0;
+    let len = queue.length;
+    for (let i = 0; i < len; i++) {
+      let node = queue.shift();
+      sum += node.val;
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+    average.push(sum / len);
+  }
+  return average;
+};
