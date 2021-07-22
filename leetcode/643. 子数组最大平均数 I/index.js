3e50530deaf4adce52736b888328cd5647ec1120
7287vencent <1327920149@qq.com>
2021-07-22 20:17:07 +0800

    feat(leetcode): 643

---
+++ leetcode/643. 子数组最大平均数 I/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function (nums, k) {
+  let sum = 0;
+  for (let i = 0; i < k; i++) {
+    sum += nums[i];
+  }
+  let maxSum = sum;
+  for (let i = k; i < nums.length; i++) {
+    sum = sum - nums[i - k] + nums[i];
+    maxSum = Math.max(maxSum, sum);
+  }
+  return maxSum / k;
+};
