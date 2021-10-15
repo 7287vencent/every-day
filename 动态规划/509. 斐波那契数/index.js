5182ee0bac6c9e54af07bc9d2a66ce274e3c1126
7287vencent <48015638+7287vencent@users.noreply.github.com>
2021-10-15 20:38:44 +0800

    feat(leetcode): 509

---
+++ 动态规划/509. 斐波那契数/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+  if (n < 2) {
+    return n;
+  }
+  let p = 0,
+    q = 0,
+    r = 1;
+  for (let i = 2; i <= n; i++) {
+    p = q;
+    q = r;
+    r = p + q;
+  }
+  return r;
+};
