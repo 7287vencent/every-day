96315ad8d718863f183ffce0a2c6990357e5caeb
7287vencent <48015638+7287vencent@users.noreply.github.com>
2021-11-03 20:44:40 +0800

    feat(leetcode): 164

---
+++ leetcode/264. 丑数 II/index.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nthUglyNumber = function (n) {
+  let dp = Array(n + 1).fill(0);
+  dp[1] = 1;
+  let p2 = 1,
+    p3 = 1,
+    p5 = 1;
+  for (let i = 2; i <= n; i++) {
+    let num2 = dp[p2] * 2,
+      num3 = dp[p3] * 3,
+      num5 = dp[p5] * 5;
+    dp[i] = Math.min(num2, num3, num5);
+    if (dp[i] === num2) {
+      p2++;
+    }
+    if (dp[i] === num3) {
+      p3++;
+    }
+    if (dp[i] === num5) {
+      p5++;
+    }
+  }
+  return dp[n];
+};
