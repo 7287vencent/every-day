69905918b71bc4f8ec27447b2466ec8934f7965f
7287vencent <48015638+7287vencent@users.noreply.github.com>
2021-11-23 20:43:22 +0800

    feat(leetcode): 377

---
+++ leetcode/377. 组合总和 Ⅳ/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var combinationSum4 = function (nums, target) {
+  let dp = Array(target + 1).fill(0);
+  dp[0] = 1;
+  for (let i = 1; i <= target; i++) {
+    for (let num of nums) {
+      if (num <= i) {
+        dp[i] += dp[i - num];
+      }
+    }
+  }
+  return dp[target];
+};
