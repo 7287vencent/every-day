28889de9426808c6c22add81bfe6bfe379b71988
7287vencent <48015638+7287vencent@users.noreply.github.com>
2021-11-29 20:12:52 +0800

    feat(面试题): 01.06

---
+++ 程序员面试金典6/面试题 01.06. 字符串压缩/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var compressString = function (S) {
+  let count = 1;
+  let str = "";
+  for (let i = 0; i < S.length; i++) {
+    if (S[i] === S[i + 1]) {
+      count++;
+    } else {
+      str += S[i] + count;
+      count = 1;
+    }
+  }
+  return S.length > str.length ? str : S;
+};
