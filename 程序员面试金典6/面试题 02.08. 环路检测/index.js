7110c93471a1a9e961e0a5062c566851ed9e62cf
7287vencent <48015638+7287vencent@users.noreply.github.com>
2021-12-04 20:15:22 +0800

    feat(leetcode): 02.08

---
+++ 程序员面试金典6/面试题 02.08. 环路检测/index.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+  if (!head || !head.next) {
+    return null;
+  }
+  let fast = head;
+  let slow = head;
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+    if (fast === slow) {
+      break;
+    }
+  }
+  if (fast !== slow) {
+    return null;
+  }
+  fast = head;
+  while (fast !== slow) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  return fast;
+};
