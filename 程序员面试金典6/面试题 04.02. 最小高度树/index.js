2643f5ee7d46f78ba8b167c0a5f377858487eaf3
7287vencent <48015638+7287vencent@users.noreply.github.com>
2021-12-05 20:43:09 +0800

    feat(leetcode): 0402

---
+++ 程序员面试金典6/面试题 04.02. 最小高度树/index.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+  if (!nums.length) {
+    return null;
+  }
+  let mid = (nums.length / 2) >> 0;
+  let root = new TreeNode(nums[mid]);
+  root.left = sortedArrayToBST(nums.slice(0, mid));
+  root.right = sortedArrayToBST(nums.slice(mid + 1));
+  return root;
+};
