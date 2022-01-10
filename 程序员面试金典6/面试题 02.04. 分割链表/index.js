b9a0bf22b30694acbf2631c38d67507fc991a28a
7287vencent <48015638+7287vencent@users.noreply.github.com>
2022-01-10 20:24:55 +0800

    feat(leetcode): 02.04

---
+++ 程序员面试金典6/面试题 02.04. 分割链表/index.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function (head, x) {
+  if (!head) {
+    return head;
+  }
+  let pre = head;
+  let cur = head;
+  while (cur) {
+    if (cur.val < x) {
+      // ? cur 和 pre 交换
+      let temp = cur.val;
+      cur.val = pre.val;
+      pre.val = temp;
+      pre = pre.next;
+    }
+    cur = cur.next;
+  }
+  return head;
+};
