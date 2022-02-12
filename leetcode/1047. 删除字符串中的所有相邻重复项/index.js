3b1f878d2a2193b176e9d7699a8beacd46bf6cab
7287vencent <48015638+7287vencent@users.noreply.github.com>
2022-02-12 18:47:02 +0800

    feat(leetcode): 1047

---
+++ leetcode/1047. 删除字符串中的所有相邻重复项/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function (s) {
+  const stk = [];
+  for (const ch of s) {
+    if (stk.length && stk[stk.length - 1] === ch) {
+      stk.pop();
+    } else {
+      stk.push(ch);
+    }
+  }
+  return stk.join("");
+};
