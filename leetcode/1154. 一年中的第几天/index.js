ab3264702883b723d39820a73227ac0e2781693a
7287vencent <48015638+7287vencent@users.noreply.github.com>
2022-02-14 20:37:14 +0800

    feat(leetcode): 1154

---
+++ leetcode/1154. 一年中的第几天/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} date
+ * @return {number}
+ */
+var dayOfYear = function (date) {
+  const year = +date.slice(0, 4);
+  const month = +date.slice(5, 7);
+  const day = +date.slice(8);
+
+  const amount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+    ++amount[1];
+  }
+
+  let ans = 0;
+  for (let i = 0; i < month - 1; ++i) {
+    ans += amount[i];
+  }
+  return ans + day;
+};
