d9bc4d8bddf385c65a5e5e513d8a4276ef76a193
7287vencent <48015638+7287vencent@users.noreply.github.com>
2022-02-28 18:46:11 +0800

    feat(leetcode): 1446

---
+++ leetcode/1446. 连续字符/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxPower = function (s) {
+  let ans = 1,
+    cnt = 1;
+  for (let i = 1; i < s.length; i++) {
+    if (s[i] === s[i - 1]) {
+      cnt++;
+      ans = Math.max(ans, cnt);
+    } else {
+      cnt = 1;
+    }
+  }
+  return ans;
+};
