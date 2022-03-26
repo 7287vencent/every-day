6c0534a86c1a1d81f6d50892cc133c3221c23971
7287vencent <48015638+7287vencent@users.noreply.github.com>
2022-03-26 20:05:34 +0800

    feat(leetcode): 1534

---
+++ leetcode/1534. 统计好三元组/index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} arr
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+var countGoodTriplets = function (arr, a, b, c) {
+  const n = arr.length;
+  let cnt = 0;
+  for (let i = 0; i < n; ++i) {
+    for (let j = i + 1; j < n; ++j) {
+      for (let k = j + 1; k < n; ++k) {
+        if (
+          Math.abs(arr[i] - arr[j]) <= a &&
+          Math.abs(arr[j] - arr[k]) <= b &&
+          Math.abs(arr[i] - arr[k]) <= c
+        ) {
+          ++cnt;
+        }
+      }
+    }
+  }
+  return cnt;
+};
