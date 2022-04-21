74aa2cfb9b3f2362b443bc17f07a816989f68135
7287vencent <48015638+7287vencent@users.noreply.github.com>
2022-04-21 20:43:19 +0800

    feat(leetcode): 674

---
+++ 字节跳动最爱考的64道算法题/674. 最长连续递增序列/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLengthOfLCIS = function (nums) {
+  let ans = 1;
+  let count = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[i - 1]) {
+      count++;
+    } else {
+      count = 1;
+    }
+    ans = Math.max(ans, count);
+  }
+  return ans;
+};
