812d65933006c2532325b9be5be4c885ba26c27f
7287vencent <48015638+7287vencent@users.noreply.github.com>
2022-06-05 18:15:01 +0800

    feat(leetcode): 010

---
+++ 字节跳动最爱考的64道算法题/剑指 Offer II 010. 和为 k 的子数组/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function (nums, k) {
+  const mp = new Map();
+  mp.set(0, 1);
+  let count = 0,
+    pre = 0;
+  for (const x of nums) {
+    pre += x;
+    if (mp.has(pre - k)) {
+      count += mp.get(pre - k);
+    }
+    if (mp.has(pre)) {
+      mp.set(pre, mp.get(pre) + 1);
+    } else {
+      mp.set(pre, +1);
+    }
+  }
+  return count;
+};
